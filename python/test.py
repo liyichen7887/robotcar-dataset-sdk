b1357c0d81c5c5a65501a0a3efad403935cce323
liyichen7887 <liyichen7887@gmail.com>
2017-06-11 21:03:29 -0700

    first test

--- python/test.py
+++ python/test.py
@@ -1,5 +1,5 @@
 from PIL import Image
-from image import sdk_im
-im_arr = sdk_im.load_image("/home/tusimple-sudo/Downloads/sample/stereo/left/")
+import image as sdk_im
+im_arr = sdk_im.load_image("/home/tusimple-sudo/Downloads/sample/stereo/left/1418381798076682.png")
 im = Image.fromarray(im_arr)
 im.save("test.png")
