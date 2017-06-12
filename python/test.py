e207378484e6276066c31463eb82c16f84c8c4b2
liyichen7887 <liyichen7887@gmail.com>
2017-06-11 21:06:20 -0700

    undistort

--- python/test.py
+++ python/test.py
@@ -1,5 +1,8 @@
 from PIL import Image
 import image as sdk_im
+import camera_model as camera
+
 im_arr = sdk_im.load_image("/home/tusimple-sudo/Downloads/sample/stereo/left/1418381798076682.png")
+im_arr = camera.undistort(im_arr)
 im = Image.fromarray(im_arr)
 im.save("test.png")
