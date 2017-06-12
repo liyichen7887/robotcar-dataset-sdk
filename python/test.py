dcc43e57b129c2f6d5acd47f8f4d0ca73e73e712
liyichen7887 <liyichen7887@gmail.com>
2017-06-11 21:18:15 -0700

    a

--- python/test.py
+++ python/test.py
@@ -3,6 +3,7 @@ import image as sdk_im
 import camera_model as camera
 
 im_arr = sdk_im.load_image("/home/tusimple-sudo/Downloads/sample/stereo/left/1418381798076682.png")
-im_arr = camera.undistort(im_arr)
+mode = CameraModel("../models/stereo_narrow_left.txt", "/home/tusimple-sudo/Downloads/sample/stereo/left/")
+im_arr = model.undistort(im_arr)
 im = Image.fromarray(im_arr)
 im.save("test.png")
